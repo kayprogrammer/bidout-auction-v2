f5cf7bd9c91dcdfb18355b5769e9f6b3420b5b4b
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-07-08 16:52:17 +0100

    updated main.py

--- app/main.py
+++ app/main.py
@@ -24,57 +24,50 @@ from jinja2 import Environment, PackageLoader
 
 env = Environment(loader=PackageLoader("app", "templates"))
 
+app = Sanic(name=settings.PROJECT_NAME)
 
-def create_app() -> Sanic:
-    app = Sanic(name=settings.PROJECT_NAME)
-
-    # SWAGGER DOCS CONFIG
-    app.extend(
-        config=Config(
-            oas_ui_default="swagger",
-            oas_url_prefix="/",
-        )
+# SWAGGER DOCS CONFIG
+app.extend(
+    config=Config(
+        oas_ui_default="swagger",
+        oas_url_prefix="/",
     )
-    app.ext.openapi.describe(
-        f"{settings.PROJECT_NAME} API",
-        version="2",
-        description="This is a simple Auction API",
-    )
-    app.ext.openapi.add_security_scheme(
-        "token",
-        "http",
-        scheme="bearer",
-        bearer_format="JWT",
-    )
-    app.ext.openapi.secured("token")
-    # --------------------------
-    # REGISTER MIDDLEWARES
-    app.register_middleware(add_cors_headers, "response", priority=99)
-    app.register_middleware(inject_db_session, "request")
-    app.register_middleware(close_db_session, "response")
-    app.register_middleware(inject_current_user, "request")
-    app.register_middleware(inject_or_remove_session_key, "response")
+)
+app.ext.openapi.describe(
+    f"{settings.PROJECT_NAME} API",
+    version="2",
+    description="This is a simple Auction API",
+)
+app.ext.openapi.add_security_scheme(
+    "token",
+    "http",
+    scheme="bearer",
+    bearer_format="JWT",
+)
+app.ext.openapi.secured("token")
+# --------------------------
+# REGISTER MIDDLEWARES
+app.register_middleware(add_cors_headers, "response", priority=99)
+app.register_middleware(inject_db_session, "request")
+app.register_middleware(close_db_session, "response")
+app.register_middleware(inject_current_user, "request")
+app.register_middleware(inject_or_remove_session_key, "response")
 
-    # EXCEPTION HANDLERS
-    app.error_handler.add(Exception, sanic_exceptions_handler)
-    app.error_handler.add(ValidationError, validation_exception_handler)
+# EXCEPTION HANDLERS
+app.error_handler.add(Exception, sanic_exceptions_handler)
+app.error_handler.add(ValidationError, validation_exception_handler)
 
-    # REGISTER BLUEPRINTS
-    app.blueprint(auth_router)
-    app.blueprint(listings_router)
-    app.blueprint(auctioneer_router)
-    app.blueprint(general_router)
+# REGISTER BLUEPRINTS
+app.blueprint(auth_router)
+app.blueprint(listings_router)
+app.blueprint(auctioneer_router)
+app.blueprint(general_router)
 
-    # TEMPLATES CONFIG FOR EMAILS
-    app.ctx.template_env = env
+# TEMPLATES CONFIG FOR EMAILS
+app.ctx.template_env = env
 
-    # EXTRA CONFIGS
-    app.config.SECRET = settings.SECRET_KEY
-    return app
-
-
-app = create_app()
-
+# EXTRA CONFIGS
+app.config.SECRET = settings.SECRET_KEY
 
 @openapi.definition(
     tag="HealthCheck",
