957fc180607b28a635317451da1b3ec61687341a
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-07-09 20:54:26 +0100

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+pip install --upgrade pip
+
+
+
+export PYTHONPATH=$PWD
+
+#install dependencies
+pip install -r requirements.txt
+
+# Initialize database
+python initials/db_starter.py
+
+# Run migrations
+alembic upgrade heads
+
+# Create initial data
+python initials/initial_data.py
--- initials/initial.sh
+++ initials/initial.sh
@@ -1,10 +1,4 @@
 #! /usr/bin/env bash
-pip install --upgrade pip
-
-export PYTHONPATH=$PWD
-
-#install dependencies
-pip install -r requirements.txt
 
 # Initialize database
 python initials/db_starter.py
