8488930cd867fe0cb3e7eefe1a992899857e640c
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-11-24 12:24:24 +0100

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,14 +8,11 @@ export PYTHONPATH=$PWD
 # #install dependencies
 # pip install -r requirements.txt
 
-# # Initialize database
-# python initials/db_starter.py
+# Initialize database
+python initials/db_starter.py
 
 # Run migrations
 alembic upgrade heads
 
 # Create initial data
 python initials/initial_data.py
-if [ -f .env ]; then
-    source .env
-fi
